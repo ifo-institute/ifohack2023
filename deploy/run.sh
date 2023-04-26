5b8b6e6127a4aafed2ac774d12ae8a3a239548d7
muskuloes <tuekam@ifo.de>
2023-04-26 00:43:10 +0000

    update run script

--- deploy/run.sh
+++ deploy/run.sh
@@ -1,14 +1,5 @@
 #!/usr/bin/env bash
 
-# A prerequisite for this script is that miniconda is installed on the server.
-# source $HOME/miniconda3/etc/profile.d/conda.sh
-
 set -x
 
-PROJECT="$HOME/ifohack2023"
-
-docker build -t leaderboard $PROJECT/Leaderboard
-docker build -t streamlit-workshop "$PROJECT/Workshops/Streamlit Basics and VSCode/"
-
-docker run -d -p 8081:8081 leaderboard
-docker run -d -p 8080:8080 streamlit-workshop
+docker compose up --build -d
