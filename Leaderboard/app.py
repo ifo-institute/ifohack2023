69fe00b07fe6d86ae17784bceb2baf79ed7ea294
muskuloes <vic.tuekam@gmail.com>
2023-04-27 18:47:57 +0200

    update app

--- Leaderboard/app.py
+++ Leaderboard/app.py
@@ -41,26 +41,29 @@ if os.path.isfile(filename):
     df["score"] = df["score"].round(2)
     df.sort_values(by="score", ascending=False, inplace=True, ignore_index=True)
 
-    first_place = df.iloc[0]
-    second_place = df.iloc[1]
-    third_place = df.iloc[2]
+    n_submissions = len(df)
 
-    col1, col2, col3 = st.columns(3)
-    col1.metric(
-        label=f":first_place_medal: : **{first_place['team']}**",
-        value=first_place["score"],
-        delta=round(first_place["score"] - second_place["score"], 2),
-    )
-    col2.metric(
-        label=f":second_place_medal: : **{second_place['team']}**",
-        value=second_place["score"],
-        delta=round(second_place["score"] - first_place["score"], 2),
-    )
-    col3.metric(
-        f":third_place_medal: : **{third_place['team']}**",
-        value=third_place["score"],
-        delta=round(third_place["score"] - first_place["score"], 2),
-    )
+    first_place = df.iloc[0]
+    if n_submissions >= 3:
+        second_place = df.iloc[1]
+        third_place = df.iloc[2]
+
+        col1, col2, col3 = st.columns(3)
+        col1.metric(
+            label=f":first_place_medal: : **{first_place['team']}**",
+            value=first_place["score"],
+            delta=round(first_place["score"] - second_place["score"], 2),
+        )
+        col2.metric(
+            label=f":second_place_medal: : **{second_place['team']}**",
+            value=second_place["score"],
+            delta=round(second_place["score"] - first_place["score"], 2),
+        )
+        col3.metric(
+            f":third_place_medal: : **{third_place['team']}**",
+            value=third_place["score"],
+            delta=round(third_place["score"] - first_place["score"], 2),
+        )
 
     st.dataframe(
         df.style.format(subset=["score"], formatter="{:.2f}"),
